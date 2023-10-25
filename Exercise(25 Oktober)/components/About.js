d4a23a260d451120e6b049ff0dde7baaf397b070
ShawnLondah <133621934+shawn949494@users.noreply.github.com>
2023-10-25 12:21:50 +0800

    Add files via upload

---
+++ Exercise(25 Oktober)/components/About.js
@@ -0,0 +1,13 @@
+// about page
+function About () {
+    return (
+        <div>
+            <h2> Frond-end web development is the development of the graphical user interface of HTML, CSS, and JavaScript, so that users can view and interact with that website. </h2>
+
+            Read more the article :
+            <a href="https://frontendmasters.com/guides/frond-end-handbook/2019/">
+                https://frontendmasters.com/guides/frond-end-handbook/2019
+            </a>
+        </div>
+    );
+}
